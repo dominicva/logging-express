05aa684af71aa25272b52feee0016c2a66493868
Dominic <dominicvana@gmail.com>
2022-05-06 18:45:25 -0500

    Fix: change to chalk v4 to use commonJS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 const genLogger = require('./utils/gen_numbered_logger');
 const welcomeLog = require('./utils/welcome_log');
+const data = require('./data');
 const composeDataWith = require('./utils/req_data_compose');
 
 function reqLogger(req) {
   welcomeLog();
-  const messageBuilder = composeDataWith(req);
-  const messages = messageBuilder(logData);
+  const buildMessages = composeDataWith(req);
+  const messages = buildMessages(data);
 
   const customLog = genLogger();
   for (const message of messages) {
